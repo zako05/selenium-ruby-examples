80e405c77e48b2ddc557efe1f0e632198e74b24a
Michal Zak <zako05@gmail.com>
2016-04-19 15:18:38 +0200

    selenium remote, selenium grid and sauce labs - part1: initial setup

---
+++ config_cloud.rb
@@ -0,0 +1,9 @@
+# filename: config_cloud.rb
+
+ENV['base_url']			||= 'http://the-internet.herokuapp.com'
+ENV['host']			||= 'saucelabs'
+# ENV['SAUCE_USERNAME']		||= 'your-sauce-username'
+# ENV['SAUCE_ACCESS_KEY']	||= 'your-sauce-access-key'
+ENV['operating_system']		||= 'windows xp'
+ENV['browser']			||= 'internet_explorer'
+ENV['browser_version']		||= '8'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,19 @@ require 'selenium-webdriver'
 RSpec.configure do |config|
 
 	config.before(:each) do
+
+		# case ENV['host']
+		# when 'saucelabs'
+		# 	caps = Selelnium::WebDriver::Remote::Capabilities.send(ENV['browser'])
+		# 	caps.version = ENV['browser_version']
+		# 	caps.platform = ENV['operating_system']
+		# 	caps[:name] = example.metadata[:full_description]
+                #
+		# 	@driver = Selenium::WebDriver.for(
+		# 		:remote,
+		# 		url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
+		# 		desired_capabilities: caps)
+		# else
 		case ENV['browser']
 		when 'chrome'
 			@driver = Selenium::WebDriver.for :chrome
